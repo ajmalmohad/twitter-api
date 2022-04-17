26e86f0d8d5191ac0d7e8ef733fc2039c07c1fdf
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-04-17 11:57:05 +0530

    Gets all followers

--- main.py
+++ main.py
@@ -59,12 +59,11 @@ def connect_follow_endpoint(url, params):
 def main():
     users_url = user_url()
     user = connect_user_endpoint(users_url)
-    data = user['data']
-    print()
-    # follow_url = followers_url(id)
-    # params = followers_params()
-    # followers = connect_follow_endpoint(follow_url, params)
-    # print(json.dumps(followers, indent=4, sort_keys=True))
+    id = user['data'][0]['id']
+    follow_url = followers_url(id)
+    params = followers_params()
+    followers = connect_follow_endpoint(follow_url, params)
+    print(json.dumps(followers, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
